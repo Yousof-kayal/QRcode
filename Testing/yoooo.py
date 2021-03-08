7eb128a15ef2f31be6484d194d6b98ae1921df06
Yousof <yak35097@marymount.edu>
2021-03-08 22:09:23 +0300

    Adding promts for customization and intuitivity

--- Testing/yoooo.py
+++ Testing/yoooo.py
@@ -3,12 +3,21 @@ import png
 import string
 import pyqrcode
 
+"""
+Prompting user to select number of QR codes to generate,
+the value entered reflects the number of loops.
+As well as the length of the QR code.
+"""
 QRamount = input("How many QR codes would you like to generate?")
 QRamount = int(QRamount)
+
+QRlength = input("How long should each QR string be?")
+QRlength = int(QRlength)
+
 """Making a list with a specified amount of random integers"""
 keylist = []
 for i in range(0,QRamount):
-    QRrandom = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    QRrandom = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(QRlength))
     keylist.append(QRrandom)
 
 """
