5c5f7985e85be6af749b169fc9435594ac0ae311
Aamer Alkazemi <aamer@expertsintech.com>
2021-03-08 12:43:53 -0800

    Added QR Decoder

---
+++ Testing/mainQR.py
@@ -0,0 +1,14 @@
+from tools import generateQR, readqrcode
+
+print("Welcome to QR Interface 0.1")
+userInput = int(input("If you would to create qr codes please press 1\nIf you would like to scan a QR please press 2\n"))
+
+if userInput == 1:
+    generateQR()
+elif userInput == 2:
+   img = input("Please enter QR image name with extension\neg: dog.py\n")
+   print(f"The QR code that has been scanned is {readqrcode(img)}")
+
+else:
+    print("Invalid Input")
+    quit()
---
+++ Testing/tools.py
@@ -0,0 +1,52 @@
+import random
+import string
+import pyqrcode
+from PIL import Image
+
+
+"""
+Function to create qr codes
+"""
+def createqr(QRamount, keylist):
+    for j in range(0, QRamount):
+        QR = pyqrcode.create(f"{keylist[j]}")
+        QR.png(f'{keylist[j]}.png', scale=8)
+#      QR.eps(f"{keylist[j]}.eps", scale=2)
+#      print(QR.terminal(quiet_zone=1))
+
+
+"""
+Function to read a QR code and return a value
+"""
+
+
+def readqrcode(imageName):
+    from pyzbar.pyzbar import decode
+    data = decode(Image.open(imageName))
+    for i in data:
+        return ((i.data.decode("utf-8")))
+
+
+
+def generateQR():
+    """
+    Prompting user to select number of QR codes to generate,
+    the value entered reflects the number of loops.
+    As well as the length of the QR code.
+    """
+
+    QRamount = int(input("How many QR codes would you like to generate?"))
+    QRlength = int(input("How long should each QR string be?"))
+
+    """Making a list with a specified amount of random integers"""
+    keylist = []
+    for i in range(0, QRamount):
+        QRrandom = ''.join(
+            random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(QRlength))
+        keylist.append(QRrandom)
+
+    """
+    Here is the QR code generator function.
+    It creates elements incrementally from the list (keylist)
+    """
+    createqr(QRamount, keylist)
